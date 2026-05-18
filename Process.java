9374275e3e90216bc83238d306c012bc354893b6
S_WAJID_A <wajid4600w@gmail.com>
2026-05-19 00:37:36 +0300

    Reformat getters and setters for consistency

--- Process.java
+++ Process.java
@@ -20,31 +20,39 @@ public class Process {
         this.waitingTime = 0;
         this.turnaroundTime = 0;
     }
-
     public static void resetCounter() {
         procCounter = 0;
-    }
+ }
 
     // Getters and Setters
-    public int getPid()                      { return pid; }
-    public String getState()                 { return state; }
-    public void setState(String state)       { this.state = state; }
-    public int getBurstTime()                { return burstTime; }
-    public int getRemainingTime()            { return remainingTime; }
-    public void setRemainingTime(int t)      { this.remainingTime = t; }
-    public int getArrivalTime()              { return arrivalTime; }
-    public int getWaitingTime()              { return waitingTime; }
-    public void setWaitingTime(int t)        { this.waitingTime = t; }
-    public int getTurnaroundTime()           { return turnaroundTime; }
-    public void setTurnaroundTime(int t)     { this.turnaroundTime = t; }
+    public int getPid()                    
+    { return pid; }
+    public String getState()              
+    { return state; }
+    public void setState(String state)     
+    { this.state = state; }
+    public int getBurstTime()               
+    { return burstTime; }
+    public int getRemainingTime()         
+    { return remainingTime; }
+    public void setRemainingTime(int t)    
+    { this.remainingTime = t; }
+    public int getArrivalTime()             
+    { return arrivalTime; }
+    public int getWaitingTime()          
+    { return waitingTime; }
+    public void setWaitingTime(int t)     
+    { this.waitingTime = t; }
+    public int getTurnaroundTime()        
+    { return turnaroundTime; }
+    public void setTurnaroundTime(int t)   
+    { this.turnaroundTime = t; }
 
     public static int getProcCounter() {
-        return procCounter;
-    }
+        return procCounter; }
 
     @Override
     public String toString() {
-        return String.format("P%d [Burst: %d, Arrival: %d, State: %s]",
-                pid, burstTime, arrivalTime, state);
+        return String.format("P%d [Burst: %d, Arrival: %d, State: %s]", pid, burstTime, arrivalTime, state);
     }
 }
